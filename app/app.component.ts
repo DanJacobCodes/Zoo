a0ca43516f2bcff599d0cc2d251e4452f8eb74a9
Dan Jacob <dansamueljacob@gmail.com>
2017-06-10 15:15:10 -0700

    add animal list component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-
+import { Animal } from './animal.model';
 
 @Component({
   selector: 'app-root',
   template: `
   <div class ="container">
-  <h1></h1>
+    <h1>{{zoo}}</h1>
+    <ul>
+      <li *ngFor="let currentAnimal of animals">{{currentAnimal.name}}<button (click)="editAnimal()">Edit</button></li>
+    </ul>
   </div>
   `
 })
@@ -13,5 +16,24 @@ import { Component } from '@angular/core';
 
 
 export class AppComponent {
+  zoo: string = "Epicodus Zoo";
+  selectedAnimal = null;
+  animals: Animal[] = [
+    new Animal('Gorilla', 'Harambe', 2, 'Carnivore', 'Gorilla enclosure', 'Male', 'Relaxing not harming anyone', 'Overreacting')
+  ];
 
+  editAnimal(clickedAnimal) {
+    this.selectedAnimal = clickedAnimal;
+  }
+
+  toggleDetails(clickedAnimal) {
+    this.animalDetails = clickedAnimal;
+  }
+  finishedEditing(){
+    this.selectedAnimal = null;
+  }
+
+  addAnimal(newAnimal: Animal){
+    this.animalList.push(newAnimal);
+  }
 }
---
+++ app/edit-animal.component.ts
@@ -0,0 +1,53 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Animal } from './animal.model';
+
+@Component_({
+  selector: 'edit-animal',
+  template: `
+  <div>
+  <div *ngIf="selectedAnimal">
+  <h4>{{selectedAnimal.name}}</h4>
+  <hr>
+  <h4>Edit Animal</h4>
+  <label>Edit Animal Name:</label>
+    <input [(ngModel)]="selectedAnimal.name" class="form-control">
+    <br>
+    <label>Edit Animal Species:</label>
+    <input [(ngModel)]="selectedAnimal.species" class="form-control">
+    <br>
+    <label>Edit Animal Age:</label>
+    <input [(ngModel)]="selectedAnimal.age" class="form-control">
+    <br>
+    <label>Edit Animal Diet:</label>
+    <input [(ngModel)]="selectedAnimal.diet" class="form-control">
+    <br>
+    <label>Edit Animal Location:</label>
+    <input [(ngModel)]="selectedAnimal.location" class="form-control">
+    <br>
+    <label>Edit Animal Caretakers:</label>
+    <input [(ngModel)]="selectedAnimal.caretakers" class="form-control">
+    <br>
+    <label>Edit Animal Sex:</label>
+    <input [(ngModel)]="selectedAnimal.sex" class="form-control">
+    <br>
+    <label>Edit Animal Likes:</label>
+    <input [(ngModel)]="selectedAnimal.likes" class="form-control">
+    <br>
+    <label>Edit Animal Dislikes:</label>
+    <input [(ngModel)]="selectedAnimal.dislikes" class="form-control">
+    <br>
+    <button (click)="doneButtonClicked()" class="btn btn-success">Done</button>
+  </div>
+</div>
+  `
+})
+
+
+export class EditAnimalComponent{
+  @Input() selectedAnimal: Animal
+  @Output() doneButtonClickedSender = new EventEmitter();
+
+  doneButtonClicked(){
+    this.doneButtonClickedSender.emit()
+  }
+}
