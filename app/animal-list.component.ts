00297b13bed90257af82d8beaaae605638a02811
Dan Jacob <dansamueljacob@gmail.com>
2017-06-10 15:48:13 -0700

    add an age pipe to filter animals by age

--- app/animal-list.component.ts
+++ app/animal-list.component.ts
@@ -1,2 +1,31 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Animal } from './animal.model';
+
+
+@Component ({
+  selector: 'animal-list',
+  template: `
+  <h3>Filter By:</h3>
+  <select (change)="onChange($event.target.value)">
+    <option value="allAnimals" selected="selected">All Kegs</option>
+    <option value="young">Empty Kegs</option>
+    <option value="mature"> Not Empty Kegs</option>
+  </select>
+  <hr>
+
+
+  `
+})
+
+
+export class AnimalListComponent {
+  @Input() childAnimalList: Animal[];
+  @Output()  = new EventEmitter();
+
+  editAnimalButton(animalToEdit: Animal)
+
+
+
+
+
+}
